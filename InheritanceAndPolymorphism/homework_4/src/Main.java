6133ece2aedda10a942e074e7ecb41dc57c96e32
vladimir_lagutin_1 <lagutin_vlad@mail.ru>
2022-01-28 16:56:47 +0300

    HW Employees 2 edition

--- InheritanceAndPolymorphism/homework_4/src/Main.java
+++ InheritanceAndPolymorphism/homework_4/src/Main.java
@@ -8,15 +8,15 @@ public class Main {
         Company company = new Company();
 
 
-        // найм 250 сотрудников
-        for (int i = 0 ; i < 180; i++) {
+        // найм сотрудников
+        for (int i = 0 ; i < 10; i++) {
             company.hire(new Operator());
         }
         List<Employee> workers = new ArrayList<>();
-        for (int i = 0; i < 80; i++) {
+        for (int i = 0; i < 10; i++) {
             workers.add(new Manager());
-            if (i%8 == 0) {
-                workers.add(new TopManager());
+            if (i%2 == 0) {
+                workers.add(new TopManager(company));
             }
         }
         company.hireAll(workers);
@@ -28,6 +28,8 @@ public class Main {
         for (int i = 0; i < employees.size()/2; i++) {
                 company.fire(employees.get(i));
         }
+
+        System.out.println("Доход компании: " + company.getIncome());
         System.out.println("Количество сотрудников после увольнения: " + company.getEmployees().size());
 
         printEmployees(company);
@@ -36,14 +38,14 @@ public class Main {
 
 
     private static void printEmployees (Company company) {
-        //15 самых высоких зарплаты
-        List<Employee> TopSalaryStaff =  company.getTopSalaryStaff(15);
+        System.out.println("20% самых высоких зарплат: ");
+        List<Employee> TopSalaryStaff =  company.getTopSalaryStaff((int) ((company.getEmployees().size())*0.2));
         for (Employee e: TopSalaryStaff ) {
                 System.out.println(e.getMonthSalary());
             }
 
-        //10 самых низких зарплат
-        List<Employee> LowSalaryStaff =  company.getLowestSalaryStaff(10);
+        System.out.println("10% самых низких зарплат: ");
+        List<Employee> LowSalaryStaff =  company.getLowestSalaryStaff((int) ((company.getEmployees().size())*0.1));
         for (Employee e: LowSalaryStaff
         ) {
             System.out.println(e.getMonthSalary());
--- InheritanceAndPolymorphism/homework_4/src/TopManager.java
+++ InheritanceAndPolymorphism/homework_4/src/TopManager.java
@@ -2,15 +2,17 @@ package src;
 
 public class TopManager implements Employee{
     private int salary = 50000;
+    private Company company;
 
-    public TopManager (){
+    public TopManager (Company company){
+        this.company = company;
 
     }
 
     @Override
     public int getMonthSalary() {
-        int income = new Company().getIncome();
-        if (income > 10000000) {
+        int income = company.getIncome();
+        if (income > 1_000_000) {
             return (int) (salary*1.5);
         }
         return salary;
