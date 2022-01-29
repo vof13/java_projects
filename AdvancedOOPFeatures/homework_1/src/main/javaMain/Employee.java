8d57d7429a7b67c8b35ce829032789a7269944a8
vladimir_lagutin_1 <lagutin_vlad@mail.ru>
2022-01-29 11:54:58 +0300

    HW Employees 2 edition

--- AdvancedOOPFeatures/homework_1/src/main/javaMain/Employee.java
+++ AdvancedOOPFeatures/homework_1/src/main/javaMain/Employee.java
@@ -1,3 +1,5 @@
+package javaMain;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
--- AdvancedOOPFeatures/homework_1/src/test/javaMain/TestSortBySalaryAndAlphabet.java
+++ AdvancedOOPFeatures/homework_1/src/test/javaMain/TestSortBySalaryAndAlphabet.java
@@ -1,6 +1,10 @@
+package javaMain;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javaMain.Employee;
+import javaMain.Main;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
@@ -11,8 +15,8 @@ public class TestSortBySalaryAndAlphabet {
     @Test
     @DisplayName("Входные данные - staff.txt")
     void sort() {
-        List<Employee> actualStaff = Employee.loadStaffFromFile("data/staff.txt");
-        List<Employee> expectedStaff = Employee.loadStaffFromFile("data/sortedStaff.txt");
+        List<Employee> actualStaff = Employee.loadStaffFromFile("D:\\java_projects\\skillbox\\java_basics\\AdvancedOOPFeatures\\homework_1\\data\\staff.txt");
+        List<Employee> expectedStaff = Employee.loadStaffFromFile("D:\\java_projects\\skillbox\\java_basics\\AdvancedOOPFeatures\\homework_1\\data\\sortedStaff.txt");
         Main.sortBySalaryAndAlphabet(actualStaff);
         assertIterableEquals(expectedStaff, actualStaff, "сортировка выполнена не по условию");
     }
