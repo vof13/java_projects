6a83174109c5adcc12bf88d22ec6fd8e9a22ca98
vof13 <lagutin_vlad@mail.ru>
2022-02-14 14:39:47 +0300

    Module 15 HW 1

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -1,6 +1,33 @@
+import java.io.File;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        long sizeInBytes = 0;
+        try {
+            File dir = new File(path);
+            sizeInBytes = calculateSizeInBytes(dir);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return sizeInBytes;
+
     }
+
+    private static Long calculateSizeInBytes(File dir) {
+        if (dir.isFile()) {
+            return dir.length();
+        }
+
+        long sum = 0;
+        File[] files = dir.listFiles();
+        for (File file : files
+        ) {
+            sum += calculateSizeInBytes(file);
+        }
+        return sum;
+    }
+
+
 }
--- FilesAndNetwork/homework_1/src/main/java/Main.java
+++ FilesAndNetwork/homework_1/src/main/java/Main.java
@@ -1,5 +1,33 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь до папки:");
+        String path = scanner.nextLine();
+        System.out.println("Размер папки " + path + " составляет "
+                + makeReadebleFormat(FileUtils.calculateFolderSize(path)));
+
+
+//        "C:\\Program Files\\Git";
+
+
+    }
+
+    private static String makeReadebleFormat(long sizeInBytes) {
+        DecimalFormat formattedDouble = new DecimalFormat("#0.0");
+        if (sizeInBytes <= 1024) {
+            return sizeInBytes + " Б";
+        } else if (sizeInBytes <= 1024 * 1024) {
+            return formattedDouble.format((double) sizeInBytes / 1024) + " Кб";
+        } else if (sizeInBytes <= 1024 * 1024 * 1024) {
+            return formattedDouble.format((double) sizeInBytes / (1024 * 1024)) + " Мб";
+        } else if (sizeInBytes <= (1024L * 1024 * 1024 * 1024)) {
+            return formattedDouble.format((double) sizeInBytes / (1024 * 1024 * 1024)) + " Гб";
+        } else {
+            return formattedDouble.format ((double) sizeInBytes / (1024L * 1024 * 1024 * 1024)) + " Тб";
+        }
 
     }
 }
