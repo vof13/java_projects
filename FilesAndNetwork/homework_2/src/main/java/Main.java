0b9100b2832ed38dcd91d4f3c5eaf2c2b69a12a8
vof13 <lagutin_vlad@mail.ru>
2022-02-15 11:12:07 +0300

    Module 15 HW 2

--- FilesAndNetwork/homework_2/src/main/java/Main.java
+++ FilesAndNetwork/homework_2/src/main/java/Main.java
@@ -1,4 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите исходный адрес копирования: ");
+        String sourceDirectory = scanner.nextLine().trim();
+        System.out.println("Введите адрес назначения: ");
+        String destinationDirectory = scanner.nextLine().trim();
+
+        FileUtils.copyFolder(sourceDirectory, destinationDirectory);
     }
 }
